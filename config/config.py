714d1313e231dd63b924d865d5f41a4956741de3
BobotheFiend <cavennnamdi@gmail.com>
2026-08-25 23:26:08 +0100

    config folder

--- config/config.py
+++ config/config.py
@@ -2,9 +2,9 @@ from sqlmodel import SQLModel, create_engine
 from urllib.parse import quote_plus
 
 # MySQL connection string
-engine = create_engine(f"mysql+pymysql://user1:"
-                       f"{quote_plus('@user112345')}"
-                       f"@localhost:3306/employee")
+engine = create_engine(f"mysql+pymysql://BobotheFiend:"
+                       f"{quote_plus('ANIAKOR1234')}"
+                       f"@127.0.0.1:3306/mini_trello_db")
 
 # Function to create database tables from SQLModel classes
 def create_db_and_tables():
