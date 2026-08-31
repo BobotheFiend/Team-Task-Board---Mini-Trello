b6e0313c77d6cf278b5c8da810d1e0f6f8be6eb4
BobotheFiend <cavennnamdi@gmail.com>
2026-08-31 14:36:17 +0100

    updated settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, create_engine
 
 class Settings(BaseSettings):
 
-    TEAM_TASK_BOARD_SCHEMA: str = "sqlite:///fallback.db"
+    TEAM_TASK_BOARD_SCHEMA: str 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
