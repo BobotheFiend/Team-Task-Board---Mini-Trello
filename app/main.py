89a1546bf4d8cf2280cd00059d29976866df58b2
cjsemicolon <cjsemicolon@gmail.com>
2026-09-01 13:46:30 +0100

    register front end

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from app.config.settings import create_db_and_tables
@@ -6,6 +7,13 @@ from app.controllers import task_controller
 
 app = FastAPI(title="Team-Task-Board Mini-Trello")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
